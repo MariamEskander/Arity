814d4b6f08bd4036d7f36940b3963d14463816e8
mariameskander <mariam.rizk62@gmail.com>
2025-07-23 15:58:07 +0300

    fixes

--- app/src/main/java/arity/calculator/App.java
+++ app/src/main/java/arity/calculator/App.java
@@ -4,6 +4,9 @@ import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
 import android.app.Application;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.instabug.library.Instabug;
+import com.instabug.library.invocation.InstabugInvocationEvent;
+
 public class App extends Application {
 
   @Override
@@ -11,6 +14,9 @@ public class App extends Application {
     super.onCreate();
 
     AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
+    new Instabug.Builder(this, "7852751d6bac7420c21180e542a03b02")
+            .setInvocationEvents(InstabugInvocationEvent.FLOATING_BUTTON, InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.SCREENSHOT)
+            .build();
 
   }
 }
